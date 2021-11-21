cf2a4b71c5da8bd46d1e4e9687583f9bb7781830
Adrian Cortes <ls-adrian@hotmail.com>
2021-11-21 10:50:58 -0500

    We add in the file main.js the function of mobile nav toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,4 +30,13 @@
       }
    }
 
+   /**
+   * Mobile nav toggle
+   */
+   on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+   })
+
 })()
