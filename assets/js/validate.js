962035cbcd77f3fc8aefa6fd3bf5c491fcc041bf
Adrian Cortes <ls-adrian@hotmail.com>
2021-12-12 13:40:36 -0500

    we remove the preventDefaul from validate.js

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -6,7 +6,6 @@ const errorMessage = document.querySelector('.error-message');
 const sentMessage = document.querySelector('.sent-message');
 
 form.addEventListener("submit", e => {
-   e.preventDefault();
    let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
    if(!emailRegex.test(email.value)){
